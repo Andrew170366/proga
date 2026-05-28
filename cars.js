410c745fff9754b062080855acd4ffac8bbbd0e3
Andrew170366 <cody1235@mail.ru>
2026-05-28 22:09:53 +0500

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githack.com/Andrew170366/proga/refs/heads/main/bmw.png",
+
+  "https://raw.githack.com/Andrew170366/proga/refs/heads/main/mers.png",
+
+  "https://raw.githack.com/Andrew170366/proga/refs/heads/main/porch.png",
+
+  "https://raw.githack.com/Andrew170366/proga/refs/heads/main/volks.png",
+
+  "https://raw.githack.com/Andrew170366/proga/refs/heads/main/skoda.png",
+
+  "https://raw.githack.com/Andrew170366/proga/refs/heads/main/audi.png"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "BMW",
+
+  "Mercedes",
+
+  "Porsche",
+
+  "Volkswagen",
+
+  "Skoda",
+
+  "Audi"
+
+];
